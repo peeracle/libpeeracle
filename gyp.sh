3103c17261e721d53ea5a0ce6ba6c387f4560df1
Axel Isouard <a.isouard@dotstar.fr>
2015-05-23 20:02:57 +0900

    Fix the path in gyp.sh

--- gyp.sh
+++ gyp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e;
 
-gyp build.gyp -f ninja --no-circular-check --check -I`pwd`/third_party/webrtc/webrtc/build/common.gypi -I`pwd`/build/common.gypi -I/Users/vm/libpeeracle/third_party/webrtc/webrtc/supplement.gypi --depth=.
+gyp build.gyp -f ninja --no-circular-check --check -I`pwd`/third_party/webrtc/webrtc/build/common.gypi -I`pwd`/third_party/webrtc/webrtc/supplement.gypi --depth=.
