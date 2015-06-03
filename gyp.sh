bc7f6d12cdfd3ac4e054a822b7a6e7a47be407e6
Axel Isouard <a.isouard@dotstar.fr>
2015-06-03 13:32:54 +0900

    build: Remove gyp.sh

--- gyp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e;
-
-gyp build.gyp -f ninja -Dconfiguration=Release --no-circular-check --check -Ithird_party/webrtc/webrtc/build/common.gypi -Ithird_party/webrtc/webrtc/supplement.gypi --depth=.
