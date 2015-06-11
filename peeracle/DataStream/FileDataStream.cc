91451ec1ef2a96b6e9bbe3dce0776098f63a3c4a
BryanOrtiz <bryan.ortiz@epitech.eu>
2015-06-11 15:50:23 +0200

    Merge changes from upstream

--- peeracle/DataStream/FileDataStream.cc
+++ peeracle/DataStream/FileDataStream.cc
@@ -25,7 +25,7 @@
 namespace peeracle {
 
 FileDataStream::FileDataStream(const DataStreamInit &dsInit)
-  : filename_(dsInit.path) { }
+  : filename_(dsInit.path), readOnly_(true) { }
 
 bool FileDataStream::open() {
   this->file_.open(this->filename_.c_str(), std::ifstream::binary);
@@ -69,6 +69,7 @@ std::streamsize FileDataStream::length() const {
 }
 
 std::streamsize FileDataStream::tell() const {
+  //  std::streamsize cursor = this->file_.tellg();
   return 0;
 }
 
@@ -85,79 +86,255 @@ std::streamsize FileDataStream::read(uint8_t *buffer,
 
   this->file_.read(reinterpret_cast<char*>(*buffer), length);
   std::streamsize s = this->file_.gcount();
+  this->file_.seekg(cursor + s);
   return s;
 }
 
 std::streamsize FileDataStream::read(int8_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+1 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<int8_t>(buffer), 1);
+  this->file_.read(static_cast<int8_t>(buffer), 1);
+  this->file_.seekg(cursor+1);
+  return 1;
 }
 
 std::streamsize FileDataStream::read(uint8_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+1 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(static_cast<uint8_t>(buffer), 1);
+  this->file_.seekg(cursor+1);
+  return 1;
 }
 
 std::streamsize FileDataStream::read(int16_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+2 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<int16_t>(buffer), 2);
+  this->file_.seekg(cursor+2);
+  return 2;
 }
 
 std::streamsize FileDataStream::read(uint16_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+2 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<uint16_t>(buffer), 2);
+  this->file_.seekg(cursor+2);
+  return 2;
 }
 
 std::streamsize FileDataStream::read(int32_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<int32_t>(buffer), 4);
+  this->file_.seekg(cursor+4);
+  return 4;
 }
 
 std::streamsize FileDataStream::read(uint32_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<uint32_t>(buffer), 4);
+  this->file_.seekg(cursor+4);
+  return 4;
 }
 
 std::streamsize FileDataStream::read(float *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<float>(buffer), 4);
+  this->file_.seekg(cursor+4);
+  return 4;
 }
 
 std::streamsize FileDataStream::read(double *buffer) {
-  return 0;
+    if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<double>(buffer), 4);
+  this->file_.seekg(cursor+4);
+  return 4;
 }
 
-std::streamsize FileDataStream::read(std::string *buffer) {
-  return 0;
-}
+std::streamsize FileDataStream::peek(uint8_t **buffer, std::streamsize length) {
+    if (!this->file_.is_open() || length < 1) {
+    return 0;
+  }
 
-std::streamsize FileDataStream::peek(uint8_t *buffer, std::streamsize length) {
-  return 0;
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor + length > this->fileSize_) {
+    length = this->fileSize_ - cursor;
+  }
+
+  this->file_.read(reinterpret_cast<char*>(*buffer), length);
+  return length;
 }
 
 std::streamsize FileDataStream::peek(int8_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+1 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<char>(buffer), 1);
+  return 1;
 }
 
 std::streamsize FileDataStream::peek(uint8_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+1 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<unsigned char>(buffer), 1);
+  return 1;
 }
 
 std::streamsize FileDataStream::peek(int16_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+2 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<int16_t>(buffer), 2);
+  return 2;
 }
 
 std::streamsize FileDataStream::peek(uint16_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+2 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<uint16_t>(buffer), 2);
+  return 2;
 }
 
 std::streamsize FileDataStream::peek(int32_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<int32_t>(buffer), 4);
+  return 4;
 }
 
 std::streamsize FileDataStream::peek(uint32_t *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<uint32_t>(buffer), 4);
+  return 4;
 }
 
 std::streamsize FileDataStream::peek(float *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<float>(buffer), 4);
+  return 4;
 }
 
 std::streamsize FileDataStream::peek(double *buffer) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  if (cursor+4 > this->fileSize_) {
+    return 0;
+  }
+
+  this->file_.read(reinterpret_cast<double>(buffer), 4);
+  return 4;
 }
 
 std::streamsize FileDataStream::peek(std::string *buffer) {
@@ -166,39 +343,102 @@ std::streamsize FileDataStream::peek(std::string *buffer) {
 
 std::streamsize FileDataStream::write(const char *buffer,
                                       std::streamsize length) {
+  if  (!this->file_.is_open()) {
   return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.write(buffer, length);
+  return length;
 }
 
 std::streamsize FileDataStream::write(int8_t value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<int8_t>(value));
+  return 1;
 }
 
 std::streamsize FileDataStream::write(uint8_t value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<unsigned_char>(value));
+  return 1;
 }
 
 std::streamsize FileDataStream::write(int16_t value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<signed_short>(value));
+  return 2;
 }
 
 std::streamsize FileDataStream::write(uint16_t value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<unsigned_short>(value));
+  return 2;
 }
 
 std::streamsize FileDataStream::write(int32_t value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<signed_int>(value));
+  return 4;
 }
 
 std::streamsize FileDataStream::write(uint32_t value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<unsigned_int>(value));
+  return 4;
 }
 
 std::streamsize FileDataStream::write(float value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<float>(value));
+  return 4;
 }
 
 std::streamsize FileDataStream::write(double value) {
-  return 0;
+  if (!this->file_.is_open()) {
+    return 0;
+  }
+
+  std::streamsize cursor = this->file_.tellg();
+  this->file_.seekp(cursor);
+  this->file_.put(reinterpret_cast<double>(value));
+  return 4;
 }
 
 std::streamsize FileDataStream::write(const std::string &value) {
