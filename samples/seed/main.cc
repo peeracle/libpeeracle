41fe07f67732182c0e4c17a8c5c73f82fe6cd318
Axel Isouard <ax@eml.cc>
2015-09-05 08:23:42 +0200

    seed: Write sample code

--- samples/seed/main.cc
+++ samples/seed/main.cc
@@ -21,11 +21,107 @@
  */
 
 #include <iostream>
+#include "peeracle/Tracker/Client/TrackerClient.h"
+#include "peeracle/DataStream/FileDataStream.h"
+#include "peeracle/Metadata/Metadata.h"
+#include "peeracle/Session/Session.h"
+#include "peeracle/Session/SessionHandle.h"
+#include "peeracle/WebSocketsClient/WebSocketsClient.h"
 #include "peeracle/peeracle.h"
+
+peeracle::Metadata metadata;
+peeracle::Session *session;
+peeracle::SessionHandle *sessionHandle;
+
+class MySessionObserver : public peeracle::SessionObserver {
+};
+
+class MySessionHandleObserver : public peeracle::SessionHandleObserver {
+};
+
+MySessionObserver sessionObserver;
+MySessionHandleObserver sessionHandleObserver;
+
+int load_metadata(const std::string &fileName) {
+  bool result;
+  peeracle::DataStreamInit init;
+  peeracle::FileDataStream *file;
+
+  init.path = fileName;
+  file = new peeracle::FileDataStream(init);
+
+  if (!file->open()) {
+    std::cout << "Can't open the file." << std::endl;
+  }
+  result = metadata.unserialize(file);
+  delete file;
+
+  return result;
+}
+
+class TrackerWebSocketsClientObserver :
+  public peeracle::WebSocketsClientObserver {
+ public:
+  TrackerWebSocketsClientObserver() {
+  }
+
+  void onConnect() {
+  }
+
+  void onMessage(const char *buffer, size_t length) {
+  }
+
+  void onDisconnect() {
+  }
+
+  void onError() {
+  }
+};
+
+class MyTrackerClientObserver : public peeracle::TrackerClientObserver {
+ public:
+  void onConnect(const std::string &id) {
+  }
+
+  void onDisconnect() {
+  }
+
+  void onConnectionError() {
+  }
+
+  void onPeerConnect(const std::string &hash, const std::string &peerId,
+                             uint32_t got, bool poke) {
+  }
+
+ protected:
+  ~MyTrackerClientObserver() { }
+};
 
 int main(int argc, char **argv) {
   peeracle::init();
-  peeracle::cleanup();
 
+  /*MyTrackerClientObserver *trackerObserver = new MyTrackerClientObserver();
+  peeracle::TrackerClientInterface *tracker =
+    new peeracle::TrackerClient("ws://127.0.0.1:8080", trackerObserver);
+
+  tracker->Init();
+  tracker->Connect();
+
+  while (tracker->Update()) {
+  }*/
+
+  if (!load_metadata(argv[1])) {
+    std::cout << "Unable to load the metadata file." << std::endl;
+    peeracle::cleanup();
+    return EXIT_FAILURE;
+  }
+
+  session = new peeracle::Session(&sessionObserver);
+  session->addMetadata(&metadata, &sessionHandleObserver);
+
+  while (session->update()) {
+  }
+
+  peeracle::cleanup();
   return EXIT_SUCCESS;
 }
